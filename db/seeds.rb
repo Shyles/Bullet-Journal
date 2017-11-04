b7c72a30b682bb7ecb84bf94a6ee48a2afb54e30
Ola L <ola.lansman@relexsolutions.com>
2017-11-04 14:22:27 -0400

    Added some styling

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+first_description="In this task, I do something. I will define this later"
+second_description="Another task"
+third_description="And another task"
+
+
+Exercise.create(description:first_description, name:"Innovative name", isForLeader:true, isForFollower:true)
+Exercise.create(description:second_description, name:"Innovative name 2", isForLeader:false, isForFollower:true)
+Exercise.create(description:third_description, name:"Innovative name 3", isForLeader:true, isForFollower:false)
