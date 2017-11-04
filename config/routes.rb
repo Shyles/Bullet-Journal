0069d8291cd4565ea7a79eed9c6c551d4ed22b03
Ola L <ola.lansman@relexsolutions.com>
2017-11-04 14:26:36 -0400

    Created ratings with scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :ratings
   resources :exercises
   root 'exercises#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
